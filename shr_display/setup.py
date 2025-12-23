d34042ada7f533f6dc302fb25ec65223876c8de5
mnakash <akashmoniruzzaman@gmail.com>
2025-12-22 21:45:21 -0500

    add voice status mapping, heartbeat liveness, and webapp toggle; remove protocol pub

--- shr_display/setup.py
+++ shr_display/setup.py
@@ -4,7 +4,7 @@ package_name = 'shr_display'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.2.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -15,9 +15,9 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='hello-robot',
-    maintainer_email='mostafa.hussein@unh.edu',
-    description='TODO: Package description',
+    maintainer='Moniruzzaman Akash',
+    maintainer_email='moniruzzaman.akash@unh.edu',
+    description='ROS 2 package that bridges display topics and ZMQ messages for the display app.',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
--- shr_display/shr_display/display_node.py
+++ shr_display/shr_display/display_node.py
@@ -1,3 +1,9 @@
+"""
+ROS 2 node that bridges display topics and ZMQ messages for the display app.
+
+Author: Moniruzzaman Akash
+"""
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
@@ -9,6 +15,15 @@ import json, yaml
 import pathlib
 import time
 import subprocess, signal, os
+
+VOICE_STATUS_TO_EXP = {
+    "IDLE": "exp:neutral",
+    "LISTENING": "exp:listening",
+    "RECORDING": "exp:recording",
+    "TRANSCRIBING": "exp:thinking",
+    "THINKING": "exp:thinking",
+    "SPEAKING": "exp:talking",
+}
 
 class DisplayNode(Node):
     def __init__(self):
@@ -32,49 +47,73 @@ class DisplayNode(Node):
         intersection_file_path = self.get_parameter('intersection_file_path').value
         webapp_file_path = self.get_parameter('webapp_file_path').value
         alive_log_interval = self.get_parameter('alive_log_interval_sec').value
-
-        # Load JSON routines
-        self.protocol_manager = ProtocolManager(
-            str(self.get_package_share_directory() / 'config' / 'protocol_routines.json'), intersection_file_path
-        )
-
-        self.zmq = ZmqInterface(tx_port, rx_port)
-        time.sleep(1) # Allow 1 second for zmq connection to establish
-
-        # ROS topics
-        self.tx_sub = self.create_subscription(String, 'display_tx', self.forward_to_app, 10)
-        self.rx_pub = self.create_publisher(String, 'display_rx', 10)
-
-        # Timers
-        self.create_timer(interval, self.publish_protocols)
-        self.create_timer(0.5, self.poll_app_responses)
-        self.create_timer(alive_log_interval, self.periodic_alive_log)
+        alive_timeout = self.get_parameter('display_alive_timeout_sec').value
+        startup_grace = self.get_parameter('display_alive_startup_grace_sec').value
+        use_webapp = self.get_parameter('use_webapp').value
 
         self.display_alive = False
+        self.display_start_time = time.monotonic()
+        self.display_last_response_time = None
+        self.display_alive_timeout = alive_timeout
+        self.display_startup_grace = startup_grace
 
-        # Publish once immediately
-        self.publish_protocols()
+        # Load JSON routines
+        # self.protocol_manager = ProtocolManager(
+        #     str(self.get_package_share_directory() / 'config' / 'protocol_routines.json'), intersection_file_path
+        # )
+
+        self.zmq = ZmqInterface(tx_port, rx_port)
+        time.sleep(2) # Allow 2 second for zmq connection to establish
+        self.zmq.send("ok")
 
         deadline = time.time() + 2
         while time.time() < deadline:
             try:
+                self.zmq.send("ok")
+                print("Sending Ok")
                 response = self.zmq.receive()
                 if response:
-                    self.display_alive = True
-                    # print(response)
+                    self.display_last_response_time = time.monotonic()
+                    self._set_display_alive(True)
+                    print(response)
                     break
-            except self.zmq.Again:
-                time.sleep(0.05)
+                time.sleep(0.5)
+            except Exception as e:
+                time.sleep(0.5)
+
+
+        # ROS topics
+        self.tx_sub = self.create_subscription(String, 'display_tx', self.forward_to_app, 10)
+        self.voice_status_sub = self.create_subscription(String, '/voice/status', self.forward_voice_status, 10)
+        self.rx_pub = self.create_publisher(String, 'display_rx', 10)
+
+        # Timers
+        # self.create_timer(interval, self.publish_protocols)
+        self.create_timer(0.5, self.poll_app_responses)
+        self.create_timer(alive_log_interval, self.periodic_alive_log)
+
+        # Publish once immediately
+        # self.publish_protocols()
+
         
+        
+        if self.display_alive:
+            self.get_logger().info("Display app is ALIVE")
 
-        msg = "Display app is ALIVE" if self.display_alive else "Display app NOT reachable"
-        self.get_logger().info(msg)
-
-        self.start_flask_app(webapp_file_path)
+        if use_webapp:
+            self.start_flask_app(webapp_file_path)
     
     def forward_to_app(self, msg: String):
         """Forward anything being published on display_tx to the ZMQ PUB socket."""
         self.zmq.send(msg.data)
+
+    def forward_voice_status(self, msg: String):
+        status = msg.data.strip().upper()
+        exp = VOICE_STATUS_TO_EXP.get(status)
+        if not exp:
+            self.get_logger().debug(f"Unknown voice status '{msg.data}'")
+            return
+        self.zmq.send(exp)
 
     def get_default_config_path(self):
         from ament_index_python.packages import get_package_share_directory
@@ -85,29 +124,49 @@ class DisplayNode(Node):
         from ament_index_python.packages import get_package_share_directory
         return pathlib.Path(get_package_share_directory('shr_display'))
 
-    def publish_protocols(self):
-        self.protocol_manager.update_protocol_json()
-        msg_string = self.protocol_manager.get_as_string()
-        self.zmq.send(msg_string)
+    # def publish_protocols(self):
+    #     self.protocol_manager.update_protocol_json()
+    #     msg_string = self.protocol_manager.get_as_string()
+    #     self.zmq.send(msg_string)
 
     def poll_app_responses(self):
         response = self.zmq.receive()
         if response:
-            self.display_alive = True
+            self.display_last_response_time = time.monotonic()
+            self._set_display_alive(True)
             self.rx_pub.publish(String(data=response))
             # update JSON if protocol_confirm
-            try:
-                data = json.loads(response)
-                if data.get("type") == "protocol_confirm":
-                    self.protocol_manager.update_confirmed(
-                        data["protocol"],
-                        bool(data["confirmed"])
-                    )
-            except Exception:
-                pass
+            # try:
+            #     data = json.loads(response)
+            #     if data.get("type") == "protocol_confirm":
+            #         self.protocol_manager.update_confirmed(
+            #             data["protocol"],
+            #             bool(data["confirmed"])
+            #         )
+            # except Exception:
+            #     pass
 
     def periodic_alive_log(self):
-        if not self.display_alive:
+        self.zmq.send("ok") # For heartbeat
+        self._refresh_display_alive()
+
+    def _refresh_display_alive(self):
+        now = time.monotonic()
+        if self.display_last_response_time is None:
+            if now - self.display_start_time < self.display_startup_grace:
+                return
+            self._set_display_alive(False)
+            return
+        if now - self.display_last_response_time > self.display_alive_timeout:
+            self._set_display_alive(False)
+
+    def _set_display_alive(self, alive: bool):
+        if self.display_alive == alive:
+            return
+        self.display_alive = alive
+        if alive:
+            self.get_logger().info("Display app is ALIVE")
+        else:
             self.get_logger().warn("Display app NOT reachable")
 
     def start_flask_app(self, webapp_file_path):
